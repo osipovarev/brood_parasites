b8f6ca03386631ed7804c8148ddc2fffcf12e547
Ekaterina Osipova <osipovarev@gmail.com>
2023-06-19 13:43:57 -0400

    added output dir option

--- run_goenrich_analysis.R
+++ run_goenrich_analysis.R
@@ -16,15 +16,17 @@ parser = ArgumentParser()
 parser$add_argument("-w", "--workdir", type="character", help="abs path to work (current) dir")
 parser$add_argument("-p", "--pcutoff", type="numeric", default=0.1, help="p-value cutoff on the input values; default=0.1")
 parser$add_argument("-g", "--genetab", type="character", help="table (.tsv) from Degenotate MK test")
-parser$add_argument("-o", "--outfile", type="character", help="GO enrich results output file name (adj p-value cutoff <.01)")
+parser$add_argument("-o", "--outdir", type="character", help="output dir name (adj p-value cutoff <.01) to put files .enrichGO.tsv")
 args = parser$parse_args()
 
 ## Parse arguments from command line
 curr_dir = args$workdir
 setwd(curr_dir)
 file_name = args$genetab
-enrich_out_file = args$outfile
+enrich_outdir = args$outfile
 pval_thresh = args$pcutoff
+
+file_df <- read.csv(file_name, header=TRUE, sep='\t')
 
 ## Perform gene set enrichment analysis (clusterProfiler). DoS > 0
 for (dos in c('pos', 'neg')) {
@@ -50,6 +52,7 @@ for (dos in c('pos', 'neg')) {
   )
   
   enrich_result = enrich_res@result
+  enrich_out_file = paste0(enrich_outdir, dos, '.enrichGO.tsv')
   write.table(enrich_result[enrich_result$pvalue < 0.01, ], row.names=F, quote=F, file=enrich_out_file, sep="\t")
 }
 
